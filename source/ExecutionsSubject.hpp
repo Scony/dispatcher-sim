fff7923057cd285289bdc4220bbc29fc7a639bda
Pawel Lampe <pawel.lampe@gmail.com>
2017-12-05 22:23:50 +0100

    fix

--- source/ExecutionsSubject.hpp
+++ source/ExecutionsSubject.hpp
@@ -7,10 +7,10 @@
 class ExecutionsSubject
 {
 public:
-  void subscribe(std::shared_ptr<IExecutionsListener> listener);
-  void unsubscribe(std::shared_ptr<IExecutionsListener> listener);
+  virtual void subscribe(std::shared_ptr<IExecutionsListener> listener);
+  virtual void unsubscribe(std::shared_ptr<IExecutionsListener> listener);
 
-  void notify(const std::pair<long long, OperationSP>& notification);
+  virtual void notify(const std::pair<long long, OperationSP>& notification);
 
 private:
   std::vector<std::shared_ptr<IExecutionsListener> > mSubscribers;
