5ffce32c722f5ff2138bcfb096ea84c94c292423
Pawel Lampe <pawel.lampe@gmail.com>
2018-01-23 22:16:27 +0100

    tuple sorting test - just to make sure

---
+++ test/MiscellaneousTests.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+#include "Fuzzy3.hpp"
+
+TEST(MiscellaneousTests, TestTupleSorting) {
+  std::vector<std::tuple<int, int, int> > vec = {
+    {3,3,3},
+    {2,3,3},
+    {1,3,3},
+    {3,2,3},
+    {3,1,3},
+    {3,2,2},
+    {3,2,1},
+  };
+  std::vector<std::tuple<int, int, int> > expectedVec = {
+    {1,3,3},
+    {2,3,3},
+    {3,1,3},
+    {3,2,1},
+    {3,2,2},
+    {3,2,3},
+    {3,3,3},
+  };
+
+  std::sort(vec.begin(), vec.end());
+
+  EXPECT_EQ(vec, expectedVec);
+}
