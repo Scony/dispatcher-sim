f2712cf97e1bfc27efb1a4e2a040f8f738320e9c
Pawel Lampe <pawel.lampe@gmail.com>
2018-02-02 23:55:56 +0100

    colors on gantt

--- src/helpers/gantt/main.py
+++ src/helpers/gantt/main.py
@@ -1,30 +1,67 @@
 import sys
+import random
 
 import matplotlib.pyplot as plt
+import matplotlib.colors
 
+
+# color generator
+def colors():
+    # h = 0
+    # while True:
+    #     yield matplotlib.colors.hsv_to_rgb((h/1000, 1.0, 1.0))
+    #     h = (h + 100) % 1000
+
+    # while True:
+    #     yield (random.random(),
+    #            random.random(),
+    #            random.random())
+
+    colors = [x['color'] for x in matplotlib.rcParams['axes.prop_cycle']]
+    while True:
+        for color in colors:
+            yield color
 
 # read instance
 assert len(sys.argv) == 2, 'please provide path to instance file as an argument'
 with open(sys.argv[1], 'r') as fh:
     file_lines = fh.read().strip().split('\n')
-    operations = [line.split() for line in file_lines if len(line.split()) == 4]
-    operations = [[int(field) for field in operation] for operation in operations]
-    operations = [(id, duration) for [id, _, __, duration] in operations]
-    operations = dict(operations)
 
+    operations = []
+    job_id = -1
+    for line in file_lines:
+        parts = line.split()
+        if len(parts) == 3:
+            job_id = int(parts[0])
+        if len(parts) == 4:
+            [operation_id, _, __, duration] = parts
+            operations.append((job_id, int(operation_id), int(duration)))
+
+    operation_durations = dict([(operation_id, duration) for (_, operation_id, duration) in operations])
+    operation_parents = dict([(operation_id, job_id) for (job_id, operation_id, _) in operations])
 
 # read output of simulator
 input_lines = sys.stdin.read().strip().split('\n')
 assignations = [line.split() for line in input_lines]
-assignations = [(int(machine_id),
-                 int(finish_timestamp) - operations[int(operationId)],
-                 float(finish_timestamp) - 0.3) for [finish_timestamp,
-                                                     operationId,
-                                                     machine_id] in assignations]
-machine_ids, begin_timestamps, finish_timestamps = zip(*assignations)
+assignations = [(operation_parents[int(operation_id)],
+                 int(machine_id),
+                 int(end_timestamp) - operation_durations[int(operation_id)],
+                 int(end_timestamp)) for [end_timestamp,
+                                          operation_id,
+                                          machine_id] in assignations]
 
-# plot (# https://stackoverflow.com/questions/21323032/python-plot-gantt-chart)
-plt.hlines(machine_ids, begin_timestamps, finish_timestamps, colors="red", lw=8)
+# plot
+color_generator = colors()
+for job_id in sorted(set(operation_parents.values())):
+    job_assignations = [(machine_id, begin_timestamp, end_timestamp) for
+                        (parent_id, machine_id, begin_timestamp, end_timestamp) in
+                        assignations if
+                        job_id == parent_id]
+
+    machine_ids, begin_timestamps, end_timestamps = zip(*job_assignations)
+
+    plt.hlines(machine_ids, begin_timestamps, end_timestamps, colors=next(color_generator), lw=8)
+
 plt.margins(0.1)
 plt.grid(True)
 plt.show()
