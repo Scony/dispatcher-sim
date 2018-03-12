65fbf83a4382a64c796426b95628eeadc728517e
Pawel Lampe <pawel.lampe@gmail.com>
2018-03-12 21:26:08 +0100

    instance V2 generator

---
+++ src/helpers/translator_v1_to_v2/Job.hpp
@@ -0,0 +1,1 @@
+../../simulator/Job.hpp
---
+++ src/helpers/translator_v1_to_v2/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "Input.hpp"
+
+int main(int argc, char** argv)
+{
+  srand(time(0));
+
+  std::map<unsigned, unsigned> capacityToShare = { // TODO: retrieve from argv
+    { 1, 1600 },
+    { 4, 240 },
+  };
+
+  std::vector<std::pair<unsigned, unsigned> > capacitiesWCumulatedShares = {{0, 0}};
+  for (const auto& kv : capacityToShare)
+    capacitiesWCumulatedShares.emplace_back(kv.first,
+					    capacitiesWCumulatedShares.back().second + kv.second);
+
+  auto capacity = [&](unsigned x) {
+    for (const auto pair : capacitiesWCumulatedShares)
+      if (x <= pair.second)
+	return (int)pair.first;
+    return -1;
+  };
+
+  Input input;
+  input.readFromStdin();
+
+  std::cout << 0 << std::endl;
+  std::cout << 2 << std::endl;
+  std::cout << std::endl;
+
+  std::cout << input.getJobsNum() << std::endl;
+  std::cout << input.getOperationsNum() << std::endl;
+
+  for (const auto& job : input.getJobs())
+    for (const auto& operation : job->operations)
+      std::cout << job->id << " "
+		<< job->priority << " "
+		<< job->arrivalTimestamp << " "
+		<< operation->id << " "
+		<< operation->name << " "
+		<< operation->result << " "
+		<< operation->duration << " "
+		<< capacity((rand() % capacitiesWCumulatedShares.back().second) + 1)
+		<< std::endl;
+
+  return 0;
+}
