741ae381d587de4c80178472e2291ded4f39f973
Pawel Lampe <pawel.lampe@gmail.com>
2018-05-31 20:33:54 +0200

    SA Capacity Scheduler vol. 2

--- src/simulator/SACapacityScheduler.cpp
+++ src/simulator/SACapacityScheduler.cpp
@@ -18,19 +18,57 @@ void SACapacityScheduler::schedule(CapacitySchedule & schedule, JobSP job)
   SJCapacityScheduler sj(mInput, mMachines, mEstimator);
   sj.schedule(schedule, job);
 
+  bool swap;
+  std::tuple<unsigned, unsigned, unsigned, unsigned> prevMove;
+  bool initialized = false;
+  long long prevFlow;
+  long long flow;
+
   auto costFunction = [&](const CapacitySchedule& solution) {
-    return Solution::evalTotalFlow(solution.simulateDispatch(job->arrivalTimestamp, mEstimator));
+    if (!initialized)
+    {
+      flow = Solution::evalTotalFlow(solution.simulateDispatch(job->arrivalTimestamp, mEstimator));
+      initialized = true;
+    }
+    return flow;
   };
-  auto neighbouringSolution = [&](CapacitySchedule& solution) {
+  auto invertSolution = [&](CapacitySchedule& solution) {
     if (rand() % 2 > 0)
-      utility::algorithm::random_swap(solution.schedule);
+    {
+      swap = true;
+      prevMove = utility::algorithm::random_swap(solution.schedule);
+    }
     else
-      utility::algorithm::random_move(solution.schedule);
+    {
+      swap = false;
+      prevMove = utility::algorithm::random_move(solution.schedule);
+    }
+    prevFlow = flow;
+    flow = Solution::evalTotalFlow(solution.simulateDispatch(job->arrivalTimestamp, mEstimator));
+  };
+  auto revertSolution = [&](CapacitySchedule& solution) {
+    unsigned& srcMachine = std::get<1>(prevMove);
+    unsigned& dstMachine = std::get<0>(prevMove);
+    unsigned& srcMachineOffset = std::get<3>(prevMove);
+    unsigned& dstMachineOffset = std::get<2>(prevMove);
+    if (swap)
+      utility::algorithm::deterministic_swap(solution.schedule,
+                                             srcMachine,
+                                             dstMachine,
+                                             srcMachineOffset,
+                                             dstMachineOffset);
+    else
+      utility::algorithm::deterministic_move(solution.schedule,
+                                             srcMachine,
+                                             dstMachine,
+                                             srcMachineOffset,
+                                             dstMachineOffset);
+    flow = prevFlow;
   };
 
-  auto result = algorithm::sa<CapacitySchedule, long long>(schedule,
-                                                           costFunction,
-                                                           neighbouringSolution,
-                                                           mIterations);
-  schedule = result;
+  algorithm::sa_inplace<CapacitySchedule, long long>(schedule,
+                                                     costFunction,
+                                                     invertSolution,
+                                                     revertSolution,
+                                                     mIterations);
 }
