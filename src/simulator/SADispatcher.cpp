5ed015c10711a38fbabc589c7bc328c598fbdb6d
Pawel Lampe <pawel.lampe@gmail.com>
2018-08-09 22:33:12 +0200

    SA from SJLO

--- src/simulator/SADispatcher.cpp
+++ src/simulator/SADispatcher.cpp
@@ -1,4 +1,6 @@
-#include <math.h>
+#include <cmath>
+#include <algorithm>
+#include <map>
 
 #include "SADispatcher.hpp"
 #include "Solution.hpp"
@@ -17,6 +19,22 @@ void SADispatcher::dispatch(std::vector<JobSP> jobs)
 {
   for (const auto& job : jobs)
     mQueue.insert(mQueue.end(), job->operations.begin(), job->operations.end());
+
+  std::map<long long, long long> jobWeights;
+  for (const auto& operation : mQueue)
+  {
+    if (jobWeights.find(operation->parentId) == jobWeights.end())
+      jobWeights[operation->parentId] = 0;
+    jobWeights[operation->parentId] += mEstimator->estimate(operation);
+  }
+
+  std::sort(mQueue.begin(),
+            mQueue.end(),
+            [&](OperationSP a, OperationSP b) {
+              if (a->parentId != b->parentId)
+                return jobWeights[a->parentId] > jobWeights[b->parentId]; // SJ
+              return mEstimator->estimate(a) < mEstimator->estimate(b); // LO
+            });
 
   auto costFunction = [&](const std::vector<OperationSP>& solution)
       {
